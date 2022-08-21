3ea302c3a5325dc794768b2d9f9cb78144e97943
MahnoorArif-7855 <mahnoorarif5587@gmail.com>
2022-08-21 06:13:30 -0700

    updates our menu-items to navigate withRouters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
 
 import HomePage from '../src/pages/homepage.component'
-
+import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route  exact path='/' component={HomePage}/>
+        <Route  path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
